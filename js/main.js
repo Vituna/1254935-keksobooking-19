9be34a384ea762b1d28c1fbec3a0e239743c2620
Vituna <Vituna9@bk.ru>
2020-02-01 00:15:51 +0300

    добавляет объявления на карту (доработка)

--- js/main.js
+++ js/main.js
@@ -4,6 +4,26 @@ var OFFER_TITLE = ['Сдается квартира', 'Сдается дом', '
 var ROOM_TYPE = ['palace', 'flat', 'house', 'bungalo'];
 var ADDITIONAL_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var CHECK_TIME = ['12:00', '13:00', '14:00'];
+var IMG_AVATARS = 'img/avatars/user0';
+var LocationIcon = {
+  MIN_X: 0,
+  MAX_X: 1100,
+  MIN_Y: 130,
+  MAX_Y: 630
+};
+var Rooms = {
+  MIN_ROOMS: 1,
+  MAX_ROOMS: 10
+};
+var Guest = {
+  MIN_GUEST: 1,
+  MAX_GUEST: 15
+};
+var Price = {
+  MIN_PRICE: 1000,
+  MAX_PRICE: 100000
+};
+
 var cityMap = document.querySelector('.map');
 var mapPins = document.querySelector('.map__pins');
 var mapPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
@@ -12,37 +32,39 @@ var getRandom = function (min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-var getRandomArrElement = function (arr) {
-  var element = Math.floor(Math.random() * arr.length);
-  return arr[element];
+var getOffer = function (i) {
+  var offer = {
+    'author': {
+      'avatar': IMG_AVATARS + (i + 1) + '.png'
+    },
+    'offer': {
+      'title': OFFER_TITLE[i - 1],
+      'address': 'x' + ',' + ' y',
+      'price': getRandom(Price.MIN_PRICE, Price.MAX_PRICE),
+      'type': getRandom(ROOM_TYPE),
+      'rooms': getRandom(Rooms.MIN_ROOMS, Rooms.MAX_ROOMS),
+      'guests': getRandom(Guest.MIN_GUEST, Guest.MAX_GUEST),
+      'checkin': getRandom(CHECK_TIME),
+      'checkout': getRandom(CHECK_TIME),
+      'features': getRandom(ADDITIONAL_FEATURES),
+      'description': '',
+      'photos': ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
+    },
+    'location': {
+      'x': getRandom(LocationIcon.MIN_X, LocationIcon.MAX_X),
+      'y': getRandom(LocationIcon.MIN_Y, LocationIcon.MAX_Y)
+    }
+  };
+
+  return offer;
+
 };
 
 var renderPosts = function () {
-  var posts = [];
-  for (var i = 0; i <= QUANTITY; i++) {
-    posts.push({
-      'author': {
-        'avatar': 'img/avatars/user0' + (i + 1) + '.png'
-      },
-      'offer': {
-        'title': OFFER_TITLE[i - 1],
-        'address': 'x' + ',' + ' y',
-        'price': getRandom(100, 1000000),
-        'type': getRandomArrElement(ROOM_TYPE),
-        'rooms': getRandom(1, 10),
-        'guests': getRandom(1, 15),
-        'checkin': getRandomArrElement(CHECK_TIME),
-        'checkout': getRandomArrElement(CHECK_TIME),
-        'features': getRandomArrElement(ADDITIONAL_FEATURES),
-        'description': '',
-        'photos': ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
-      },
-      'location': {
-        'x': getRandom(0, 1200),
-        'y': getRandom(130, 630)
-      }
-    });
-  }
+
+  var posts = new Array(QUANTITY).map(getOffer);
+
+
   return posts;
 };
 
@@ -72,4 +94,3 @@ var createPins = function (posts) {
 cityMap.classList.remove('map--faded');
 
 createPins(renderPosts());
-
